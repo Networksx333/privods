5f731a4c3201b8399bd243f1aa2bd077ce548b07
Networksx333 <127897894+Networksx333@users.noreply.github.com>
2024-12-12 19:37:08 +0300

    wewe

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/privod/', // Замените REPO_NAME на имя вашего репозитория
+  base: '/privods/', // Замените REPO_NAME на имя вашего репозитория
 });
